a72b0f3ed3373e2e24916b1fa8a5892aceb21b39
okaozman-create <okanozman88@gmail.com>
2024-05-29 20:36:53 +0300

    Updated components

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 //images
 import Image from "../assets/example.png";
 //icoons
-import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+// import { FaGithub, FaLinkedin, FaDribbble } from "react-icons/fa";
 //type animation
 import { TypeAnimation } from "react-type-animation";
 //motion
@@ -10,8 +10,10 @@ import { motion } from "framer-motion";
 //variants
 import { fadeIn } from "../variants";
 
+import resume from "../assets/okanozmancv-en.pdf";
+
 const Banner = () => {
-  return (
+return (
     <section
       className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
       id="home"
@@ -23,18 +25,19 @@ const Banner = () => {
             <motion.h1
               variants={fadeIn("up", 0.3)}
               initial="hidden"
-              whileInView={'show'}
-              viewport={{once:false, amount:0.7}}
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
               className="text-[55px] font-bold leading-[0.8] lg:text-[80px]"
             >
               OKAN <span>OZMAN</span>
             </motion.h1>
-            <motion.div 
-             variants={fadeIn("up", 0.4)}
-             initial="hidden"
-             whileInView={'show'}
-             viewport={{once:false, amount:0.7}}
-            className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
               <span className="text-white mr-4">I am a </span>
               <TypeAnimation
                 sequence={["Developer", 2000]}
@@ -44,58 +47,44 @@ const Banner = () => {
                 repeat={Infinity}
               />
             </motion.div>
-            <motion.p 
-             variants={fadeIn("up", 0.5)}
-             initial="hidden"
-             whileInView={'show'}
-             viewport={{once:false, amount:0.7}}
-            className="mb-8 max-w-lg mx-auto lg:mx-0">
-              Starting my software development journey in 2022, I've rapidly
-              become proficient in key web technologies such as HTML, CSS,
-              JavaScript, and React, with additional self-taught expertise in
-              Amazon Web Services. This journey has sharpened my ability to
-              think critically and solve complex problems, skills that are
-              essential for creative web and application development. I'm eager
-              to bring my determination, technical proficiency, and continuous
-              learning mindset to your projects. As a Junior Frontend Developer,
-              I am committed to delivering high-quality solutions and staying at
-              the forefront of technology trends. Let's collaborate to turn your
-              vision into reality.
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+          Hello, my name is Okan, and I am 36 years old. After years, I finally achieved my dream, and since 2022, I have been programming and writing code on my own. I know I am still at the beginning of the road, and every day I add something new to myself in terms of programming. This is a passion for me. On this page, you can find information about me. Have a nice surf!
             </motion.p>
 
-            <motion.div 
-             variants={fadeIn("up", 0.6)}
-             initial="hidden"
-             whileInView={'show'}
-             viewport={{once:false, amount:0.7}}
-            className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
-              <button className="btn btn-lg">Contact me</button>
-              <a href="#" className="text-gradient btn-link">
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <button className="btn btn-lg">
+                <a href={resume} download="resume">
+                  {" "}
+                  Download CV
+                </a>
+              </button>
+
+              {/* <a href="#" className="text-gradient btn-link">
                 My Portfolio
-              </a>
+              </a> */}
             </motion.div>
             {/* socials */}
-            <motion.div 
-             variants={fadeIn("up", 0.7)}
-             initial="hidden"
-             whileInView={'show'}
-             viewport={{once:false, amount:0.7}}
-            className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
-              <a href="#">
-                <FaGithub />
-              </a>
-              <a href="#">
-                <FaDribbble />
-              </a>
-            </motion.div>
           </div>
           {/* IMAGE */}
-          <motion.div 
-           variants={fadeIn("down", 0.5)}
-           initial="hidden"
-           whileInView={'show'}
-           viewport={{once:false, amount:0.7}}
-          className="hidden lg:flex flex-1 max-w-[320px] lg:max-w[482px]">
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w[482px]"
+          >
             {/* <img
               className="w-32 h-32 md:w-400 md:h-400 rounded-full overflow-hidden"
               src={Image}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React from "react";
+
+import { FaGithub, FaLinkedin} from "react-icons/fa";
 // images
 // import Logo from '../assets/logo.svg'
 
 const Header = () => {
-  return <header className='py-8'>
-    <div className='container mx-auto'>
-      <div className='flex justify-between '>
-        {/* logo */}
-        
-        <a href='#'>
-          <p>Okan</p>
-     
-              {/* <img className='w-28 h-auto  bg-purple-900 p-1' src={Logo} alt='' /> */}
-           
-           {/* <img width="111" height="48" viewBox="0 0 111 48" fill="none" src={Logo} alt=''/>  */}
-     
-        </a>
-        {/* button */}
-        <button className='btn btn-sm'>Work with me</button>
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between ">
+          <a href="https://www.linkedin.com/in/okan-ozman/">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/okanozman-create">
+            <FaGithub />
+          </a>
+
+          {/* <img className='w-28 h-auto  bg-purple-900 p-1' src={Logo} alt='' /> */}
+
+          {/* <img width="111" height="48" viewBox="0 0 111 48" fill="none" src={Logo} alt=''/>  */}
+        </div>
       </div>
-    </div>
-      </header>;
+    </header>
+  );
 };
 
 export default Header;
