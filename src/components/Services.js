6c09645bb94d71c41f2acbc86d9ae42fa8f8a7d8
okaozman-create <okanozman88@gmail.com>
2024-05-30 23:49:37 +0300

    updated title

--- src/components/Services.js
+++ src/components/Services.js
@@ -11,8 +11,7 @@ import { fadeIn } from "../variants";
 const services = [
   {
     name: "Full Stack Deployment with AWS",
-    description:
-      "Performing full-stack deployment using Amazon services such as Amplify, Lambda functions, API Gateways, CORS configurations, and S3 buckets.",
+    description: "Performing full-stack deployment using Amazon services.",
   },
   {
     name: "Web Development",
@@ -81,14 +80,15 @@ const Services = () => {
                 const { name, description } = service;
                 return (
                   <div
-                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                    // className="border-b border-white/20 h-auto md:h-[146px] mb-[38px] flex"
+                    className="border-b border-white/20 h-auto md:h-[146px] mb-[38px] flex flex-col md:flex-row"
                     key={index}
                   >
-                    <div className="max-w-[476px]">
-                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                    <div className="w-full md:max-w-[476px]">
+                      <h4 className="text-[20px] md:text-[24px] tracking-wider font-primary font-semibold mb-4 md:mb-6">
                         {name}
                       </h4>
-                      <p className="font-secondary leading-tight">
+                      <p className="font-secondary leading-tight mb-8">
                         {description}
                       </p>
                     </div>
