cf229a0582afc942bc756d488656128dbdf98bc5
okaozman-create <okanozman88@gmail.com>
2024-06-03 23:03:07 +0300

    updated icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,11 @@ const Header = () => {
     <header className="py-8">
       <div className="container mx-auto">
         <div className="flex gap-x-7">
-          <a href="https://www.linkedin.com/in/okan-ozman/">
+          <a href="https://www.linkedin.com/in/okan-ozman/" className="text-accent text-base  hover:text-fuchsia-200 sm:text-3xl">
             <FaLinkedin />
           </a>
           <a href="https://github.com/okanozman-create">
-            <FaGithub />
+            <FaGithub className="text-accent text-base hover:text-fuchsia-200 sm:text-3xl" />
           </a>
         </div>
       </div>
